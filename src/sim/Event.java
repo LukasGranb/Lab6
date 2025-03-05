051fbccd65c6744f62cb955f6a3f10350774538e
amund <amuknu-3@student.ltu.se>
2025-03-05 13:59:16 +0100

    Testtest

--- src/sim/Event.java
+++ src/sim/Event.java
@@ -5,7 +5,7 @@ import src.events.CarLeaves;
 
 import java.util.Random;
 
-public abstract class Event {
+public abstract class Event implements Comparable<Event> {
     private double time;
 
     public Event(double time) {
